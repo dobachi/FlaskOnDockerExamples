4939fa7f6e5e128ff9f50d184d980852dfdb212e
dobachi <dobachi1983oss@gmail.com>
2018-12-26 21:31:37 +0900

    Add an example of extensions

---
+++ docker_admin/apps/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_admin import Admin
+
+app = Flask(__name__)
+
+# set optional bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
+
+@app.route('/')
+def index():
+    return "Hello world!!"
